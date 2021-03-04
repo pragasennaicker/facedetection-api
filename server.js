b1aea7ba835cb2e0ee8762356b1fc94889de5b9d
Pragasen Naicker <123pnaicker@gmail.com>
2021-03-04 11:27:12 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+const { response } = require("express");
+
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "",
+    password: "",
+    database: "smart-brain",
+  },
+});
+
+db.select("*")
+  .from("users")
+  .then((data) => console.log(data));
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => res.send("success"));
+
+app.post("/signin", (req, res) => signin.handleSignin(req, res, db, bcrypt));
+
+app.post("/register", (req, res) =>
+  register.handleRegister(req, res, db, bcrypt)
+);
+
+app.get("/profile/:id", (req, res) => profile.handleProfileGet(req, res, db));
+
+app.put("/image", (req, res) => image.handleImage(req, res, db));
+
+app.post("/imageurl", (req, res) => image.handleApiCall(req, res));
+
+app.listen(3000, () => console.log("app is running on port 3000"));
+
+/*
+/ root route --> res = this is working
+/ signin --> POST - res = success or failure
+/ register --> POST - res = user
+/ profile/:userId --> GET - res = user
+/ image --> PUT --> res = user
+*/
