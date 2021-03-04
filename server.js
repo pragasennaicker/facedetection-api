b11fff21e808fcef247fef3d0c8a3691825e3d27
Pragasen Naicker <123pnaicker@gmail.com>
2021-03-04 21:52:20 +0200

    adding database connection

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-crystalline-91870",
-    user: "",
-    password: "",
-    database: "smart-brain",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
